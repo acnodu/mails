13c8330c424ddd852a5f6a083c12dab922d13768
acnodu <34336978+acnodu@users.noreply.github.com>
2022-11-10 14:58:21 +0100

    test

--- src/layouts/SettingsLayout.vue
+++ src/layouts/SettingsLayout.vue
@@ -7,10 +7,16 @@
   />
 
   {{ version }}
+
+  <pre>
+{{ store }}
+    </pre>
 </template>
 
 <script setup>
 import { Header } from '@/components';
 import version from '@/assets/version.json';
+import { useMainStore } from '@/stores/main';
 
+const store = useMainStore();
 </script>
