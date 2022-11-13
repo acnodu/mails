4fa10a10794d031841d1e3ea236d844aba41176f
acnodu <34336978+acnodu@users.noreply.github.com>
2022-11-13 17:13:01 +0100

    testage

--- src/components/core/offcanvas/homeOffcanvas.vue
+++ src/components/core/offcanvas/homeOffcanvas.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="offcanvasTop"
-    class="offcanvas offcanvas-top text-bg-dark"
+    class="offcanvas offcanvas-top h-100 text-white bg-black"
     tabindex="-1"
     aria-labelledby="offcanvasTopLabel"
   >
@@ -9,35 +9,62 @@
       <h5 class="offcanvas-title">
         Select domain
       </h5>
+      <i
+        id="close"
+        class="bi bi-x-circle"
+        data-bs-dismiss="offcanvas"
+        :class="[isLoading?'invisible':'']"
+      />
     </div>
 
-    <div class="offcanvas-body py-0">
-      <h6
-        v-for="(domain, index) in mailStore.domains"
-        :key="index"
-        class="row mb-3"
-        @click="setNewDomain(domain)"
-      >
-        <div class="col-1">
-          <i
-            v-if="domain === mailStore.selectedDomain"
-            class="bi bi-check-lg text-success"
-          />
+    <div class="offcanvas-body container">
+      <div class="row align-items-center">
+        <div
+          v-for="(domain, index) in mailStore.domains"
+          :key="index"
+          class="col-12"
+        >
+          <h6
+            class="mb-3 row"
+            @click="setNewDomain(domain)"
+          >
+            <div class="col-1">
+              <div
+                v-if="isLoading && domain === mailStore.selectedDomain"
+                class="spinner-grow spinner-grow-sm text-success"
+                role="status"
+              />
+
+              <i
+                v-if="!isLoading && domain === mailStore.selectedDomain"
+                class="bi bi-check-lg text-success"
+              />
+            </div>
+            <div class="col">
+              {{ domain }}
+            </div>
+          </h6>
         </div>
-        <div class="col">
-          {{ domain }}
-        </div>
-      </h6>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useMailsStore } from '@/stores/mails';
 
 const mailStore = useMailsStore();
+const isLoading = ref(false);
 
 const setNewDomain = (domain) => {
-  mailStore.setSelectedDomain(domain);
+  if (!isLoading.value) {
+    isLoading.value = true;
+
+    mailStore.setSelectedDomain(domain).then(() => {
+      document.getElementById('close').click();
+      isLoading.value = false;
+    });
+  }
 };
 </script>
